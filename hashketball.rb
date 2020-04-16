095144e8b01c0790de6a062b4f62f848e877068c
Luis Fernandez <luis.fernandez2093@gmail.com>
2020-04-16 15:44:28 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,10 +3,8 @@
 def game_hash {
   :home{
     :team_name => "Brooklyn Nets"
-      
     
     :colors => ["Black", "White"]
-      
     
     :players => [ 
       {:player_name => "Alan Anderson"
@@ -18,6 +16,7 @@ def game_hash {
        :Steals => 3
        :Blocks => 1
        :Slam_Dunks => 1},
+       
       {:player_name => "Reggier Evans"
        :Number => 30
        :Shoe => 14
@@ -27,6 +26,7 @@ def game_hash {
        :Steals => 12
        :Blocks => 12
        :Slam_Dunks => 7},
+       
        {:player_name => "Brooke Lopez"
        :Number => 11
        :Shoe => 17
@@ -36,6 +36,16 @@ def game_hash {
        :Steals => 3
        :Blocks => 1
        :Slam_Dunks => 15},
+       
+       {:player_name => "Mason Plumlee"
+       :Number => 1
+       :Shoe => 19
+       :Points => 26
+       :Rebounds => 11
+       :Assists => 6
+       :Steals => 3
+       :Blocks => 8
+       :Slam_Dunks => 5}
       
       ]
       
