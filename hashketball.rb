4f5f805daa34c08ecf2ade36754d04cbf2ba48d4
Luis Fernandez <luis.fernandez2093@gmail.com>
2020-04-16 16:02:07 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,9 +2,9 @@
 
 def game_hash 
  { home: {
-    :team_name => "Brooklyn Nets"
+    team_name: "Brooklyn Nets"
     
-    :colors => ["Black", "White"]
+    colors: ["Black", "White"]
     
     :players => [ 
       {:player_name => "Alan Anderson"
