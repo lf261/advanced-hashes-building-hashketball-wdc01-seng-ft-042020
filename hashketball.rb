eea94e6243c72b0de5ccb4ca3e6f734be198a245
Luis Fernandez <luis.fernandez2093@gmail.com>
2020-04-16 16:03:03 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -6,16 +6,16 @@ def game_hash
     
     colors: ["Black", "White"]
     
-    :players => [ 
-      {:player_name => "Alan Anderson"
-       :number => 0
-       :shoe => 16
-       :points => 22
-       :rebounds => 12
-       :assists => 12
-       :steals => 3
-       :blocks => 1
-       :slam_dunks => 1},
+    players:  [ 
+      {player_name:  "Alan Anderson"
+       number:  0
+       shoe: 16
+       points: 22
+       rebounds: 12
+       assists: 12
+       steals: 3
+       blocks: 1
+       slam_dunks: 1},
        
       {:player_name => "Reggier Evans"
        :number => 30
